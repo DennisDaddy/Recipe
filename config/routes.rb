9c5b7012322a25cd5d3bee8e551159138171de23
DennisDaddy <softteez95@gmail.com>
2017-03-16 19:58:29 +0300

    Add resources posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  root 'posts#index'
 
-  get 'posts/new'
-
-  get 'posts/edit'
-
-  get 'posts/show'
-
-  get 'posts/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
 end
