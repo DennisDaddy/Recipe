4ab6dead1799c6098cc46b3d1a204da59e68436d
DennisDaddy <softteez95@gmail.com>
2017-03-18 13:15:07 +0300

    show images on index page:

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
+	mount_uploader :picture, PictureUploader
 	belongs_to :user
+
+	private
+# Validates the size of an uploaded picture.
+def picture_size
+	if picture.size > 5.megabytes
+	errors.add(:picture, "should be less than 5MB")
+	end
+end
+
 end
