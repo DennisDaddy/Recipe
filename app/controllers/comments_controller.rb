af9392878752eefbd158e45f8caa7cb47fd2f51e
DennisDaddy <softteez95@gmail.com>
2017-03-18 13:49:08 +0300

    Add partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
+  end
+
+  def create
+     @post = Post.find(params[:id])  
+     @comment = Comment.create(params[:comment].permit(:content))
+     @comment.user_id = current_user.d
+      @comment.post_id = @post.d
+
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+     
+      render 'new'
+      
+    end
+    
   end
 
   def index
