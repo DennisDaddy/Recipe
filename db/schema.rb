abaa54c6b02201edd36a1fc0e45c50c6d1278b3d
DennisDaddy <softteez95@gmail.com>
2017-03-18 12:26:34 +0300

    migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318050820) do
+ActiveRecord::Schema.define(version: 20170318092542) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170318050820) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "picture"
   end
 
   create_table "users", force: :cascade do |t|
